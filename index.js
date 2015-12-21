5efe245592fdc04e2c9efda6ca89472b8469bfe8
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-21 14:53:07 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
+var Linkedin = require('node-linkedin')(),
+    util = require('./util.js');
+
+var pickOutputs = {
+        'id': 'id',
+        'firstName': 'firstName',
+        'headline': 'headline',
+        'lastName': 'lastName',
+        'url': 'siteStandardProfileRequest.url'
+    };
+
 module.exports = {
+
+    /**
+     * Authorize module.
+     *
+     * @param dexter
+     * @returns {*}
+     */
+    authModule: function (dexter) {
+        var accessToken = dexter.environment('linkedin_access_token');
+
+        if (accessToken)
+            return Linkedin.init(accessToken);
+
+        else
+            return false;
+    },
+
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +35,22 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var linkedIn = this.authModule(dexter),
+            fields = ['id', 'first-name', 'headline', 'last-name', 'site-standard-profile-request'];
+
+        if (!linkedIn)
+            return this.fail('A [linkedin_access_token] environment need for this module.');
+
+        linkedIn.people.me(fields, function(err, data) {
+            if (err)
+                this.fail(err);
+
+            else if (data.errorCode !== undefined)
+                this.fail(data.message || 'Error Code'.concat(data.errorCode));
+
+            else
+                this.complete(util.pickResult(data, pickOutputs));
+
+        }.bind(this));
     }
 };
